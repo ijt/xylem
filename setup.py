65c9d1351cc3cc3f1fd20195f01351c4b1c57f53
Issac Trotts <itrotts@willowgarage.com>
2012-11-08 19:40:51 -0800

    Add bare minimum script with apt support.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='xylem',
+      version='0.1',
+      description='Cross-platform package management tool',
+      author='Issac Trotts',
+      author_email='itrotts@willowgarage.com',
+      scripts=['xylem'],
+      url='http://github.com/ijt/xylem',
+      packages=['xylem'])
+
